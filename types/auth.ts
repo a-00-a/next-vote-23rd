36066ce2c1d58352d5fda8ba056e7de467929d2e
a-00-a <sy.lee.2391@gmail.com>
2026-06-24 15:11:41 +0900

    Chore: 회원가입 타입 수정 (passwordConfirm 제거, inviteCode 추가)

--- types/auth.ts
+++ types/auth.ts
@@ -1,4 +1,4 @@
-import { ApiResponse } from "./common";
+import { ApiResponse } from './common';
 
 export type Part = 'BACKEND' | 'FRONTEND';
 export type Team = 'ConX' | 'Ditda' | 'Groupeat' | 'IPX' | 'Jobdri';
@@ -22,11 +22,10 @@ export interface SignupRequest {
   name: string;
   loginId: string;
   password: string;
-  passwordConfirm: string;
   team: Team;
   part: Part;
+  inviteCode: string;
 }
 
 export type SignupResponse = ApiResponse;
 export type LogoutResponse = ApiResponse;
-
