a8370997218f69767cfd949a009e8aadc4d70185
a-00-a <sy.lee.2391@gmail.com>
2026-06-26 20:19:06 +0900

    Chore: 파트장 투표 결과 응답 타입 수정

--- types/partLeader.ts
+++ types/partLeader.ts
@@ -1,6 +1,35 @@
 import { Part, Team } from './auth';
 import { ApiResponseWrapper } from './common';
 
+export interface PartLeaderVoteRequest {
+  candidateId: number;
+}
+
+export interface PartLeaderVoteResult {
+  voteId: number;
+  candidateId: number;
+  candidateName: string;
+}
+
+export type PartLeaderVoteResponse = ApiResponseWrapper<PartLeaderVoteResult>;
+
+export interface PartLeaderResultCandidate {
+  candidateId: number;
+  name: string;
+  affiliation: string;
+  imageUrl: string | null;
+  voteCount: number;
+}
+
+export interface PartLeaderResultResult {
+  part: Part;
+  partName: string;
+  candidates: PartLeaderResultCandidate[];
+}
+
+export type PartLeaderResultResponse =
+  ApiResponseWrapper<PartLeaderResultResult>;
+
 export interface Candidate {
   userId: number;
   name: string;
@@ -14,18 +43,6 @@ export interface CandidateListResponse {
   candidates: Candidate[];
 }
 
-export interface PartLeaderVoteRequest {
-  candidateId: number;
-}
-
-export interface PartLeaderVoteResult {
-  voteId: number;
-  candidateId: number;
-  candidateName: string;
-}
-
-export type PartLeaderVoteResponse = ApiResponseWrapper<PartLeaderVoteResult>;
-
 export interface CandidateDetail {
   userId: number;
   name: string;
@@ -35,15 +52,3 @@ export interface CandidateDetail {
   introduction: string;
 }
 
-export interface PartLeaderResultResponse {
-  part: Part;
-  //votingProgress: VotingProgress;
-  voteResult: {
-    ranking: number;
-    candidateId: number;
-    name: string;
-    profileImageUrl: string | null;
-    voteCount: number;
-    isWinner: boolean;
-  }[];
-}
