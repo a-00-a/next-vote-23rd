6c895195872eab67134dd0fe9e9e795784e2ca9e
a-00-a <sy.lee.2391@gmail.com>
2026-06-27 06:14:37 +0900

    Feat: 파트장 결과 API serverHeaders 옵션 추가

--- lib/api/partLeader.ts
+++ lib/api/partLeader.ts
@@ -30,7 +30,12 @@ export const voteForPartLeader = (body: PartLeaderVoteRequest) =>
     body: JSON.stringify(body),
   });
 
-export const getPartLeaderResult = (part: Part) =>
+export const getPartLeaderResult = (
+  part: Part,
+  serverHeaders?: Record<string, string>
+) =>
   fetchApi<PartLeaderResultResponse>(
-    `${API_ENDPOINTS.PART_LEADER.RESULT}?part=${part}`
+    `${API_ENDPOINTS.PART_LEADER.RESULT}?part=${part}`,
+    {},
+    serverHeaders
   );
